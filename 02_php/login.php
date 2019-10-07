420244e22f613c0a42447a4cf558ababee70c1b2
AKM Islam <mamunjahir.com@gmail.com>
2019-10-07 00:03:29 -0400

    f

---
+++ 02_php/login.php
@@ -0,0 +1,11 @@
+<?php
+function auth($name,$pass)
+{
+    if($name=="a"){
+        return 1;
+    }
+    else{
+        return 0;
+    }
+}
+?>
--- 02_php/rpc_server.php
+++ 02_php/rpc_server.php
@@ -1,6 +1,6 @@
 <?php
-
 require_once __DIR__ . '/vendor/autoload.php';
+require("login.php");
 use PhpAmqpLib\Connection\AMQPStreamConnection;
 use PhpAmqpLib\Message\AMQPMessage;
 
@@ -9,26 +9,13 @@ $channel = $connection->channel();
 
 $channel->queue_declare('rpc_queue', false, false, false, false);
 
-function fib($n)
-{
-    if ($n == 0) {
-        return 0;
-    }
-    if ($n == 1) {
-        return 1;
-    }
-    return fib($n-1) + fib($n-2);
-}
-
 echo " [x] Awaiting RPC requests\n";
 $callback = function ($req) {
-    $n = intval($req->body);
-    echo ' [.] fib(', $n, ")\n";
-
-    $msg = new AMQPMessage(
-        (string) fib($n),
-        array('correlation_id' => $req->get('correlation_id'))
-    );
+    $n = $req->body;
+    echo ' Recieved : ', $n, "\n";
+    $myvar=auth($n,"b");
+// This is where we change the message
+    $msg = new AMQPMessage($myvar ,array('correlation_id' => $req->get('correlation_id')));
 
     $req->delivery_info['channel']->basic_publish(
         $msg,
