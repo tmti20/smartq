35a804028c4ff6bbdc0c23431cd9c313e66d7d2c
AKM Islam <mamunjahir.com@gmail.com>
2019-10-08 08:34:22 -0400

    changed

--- 02_php/send.php
+++ 02_php/send.php
@@ -3,13 +3,13 @@ require_once __DIR__ . '/vendor/autoload.php';
 use PhpAmqpLib\Connection\AMQPStreamConnection;
 use PhpAmqpLib\Message\AMQPMessage;
 
-$connection = new AMQPStreamConnection('localhost', 5672, 'guest', 'guest');
+$connection = new AMQPStreamConnection('192.168.1.103', 5672, 'test', 'test');
 $channel = $connection->channel();
 
 $channel->queue_declare('hi', false, false, false, false);
 
 $msg = new AMQPMessage('Hello World!');
-$channel->basic_publish($msg, '', 'hello');
+$channel->basic_publish($msg, '', 'hi');
 
 echo " [x] Sent 'Hello World!'\n";
 
