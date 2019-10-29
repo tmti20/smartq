94a4814f583d0baca52d6e7baecfc98be267aad6
AKM Islam <mamunjahir.com@gmail.com>
2019-10-29 16:22:12 -0400

    test

---
+++ test/account.php
@@ -0,0 +1,11 @@
+<?php
+
+//Open in Notepad++
+//CHANGE THE FILE EXTENSION TO .php
+
+$hostname = "localhost"     ; // or "sql2.njit.edu"OR"sql1.njit.edu"
+$username = "myusers" ;
+$password = "mypass" ;
+$project  = "test" ;
+
+?>
